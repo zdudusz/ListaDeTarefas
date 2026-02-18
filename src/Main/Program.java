7f5bf3e4116f44d68a0a1bd2636355636a66a2f0
zdudusz <eduardogamese8@gmail.com>
2026-02-17 22:05:14 -0300

    Criando Menu com while e classe ListaDeTarefas

--- src/Main/Program.java
+++ src/Main/Program.java
@@ -1,4 +1,33 @@
 package Main;
 
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        // metódo para chamar a data de hoje
+        LocalDateTime diaAtual = LocalDateTime.now();
+        //Menu principal
+        while(true){
+            System.out.println("O que você deseja fazer: \n 1-ADICIONAR TAREFA \n 2-REMOVER TAREFA \n 3-ALTERAR TAREFA \n 4-LISTAR TAREFAS \n 5-SAIR");
+            String opcaoUsuarioMenu = input.nextLine();
+            if(opcaoUsuarioMenu.equals("1")){}
+
+            else if(opcaoUsuarioMenu.equals("2")){}
+
+            else if(opcaoUsuarioMenu.equals("3")){}
+
+            else if(opcaoUsuarioMenu.equals("4")){}
+
+            else if(opcaoUsuarioMenu.equals("5")){
+                break;
+            }
+
+            else{
+                System.out.println("OPÇÃO INVALIDA!");
+                System.out.println("\n".repeat(2));
+            }
+        }
+    }
 }
--- src/Tarefa/ListaDeTarefas.java
+++ src/Tarefa/ListaDeTarefas.java
@@ -1,4 +1,21 @@
 package Tarefa;
 
+import javax.xml.crypto.Data;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 public class ListaDeTarefas {
+    private LocalDateTime dataInicio = LocalDateTime.now();
+    private LocalDateTime dataFinal;
+    private String tarefa;
+
+
+    public ListaDeTarefas(String _tarefa,LocalDateTime _dataFinal){
+        this.tarefa = _tarefa;
+        this.dataFinal = _dataFinal;
+    }
+
+
 }
